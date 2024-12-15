0c16b1153d0b4265e29cfdc4a303bf9c3ed823b0
leomerida15 <dimasmerida15@gmail.com>
2024-12-14 20:11:36 -0400

    low size

--- src/InputMapping/context.tsx
+++ src/InputMapping/context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'use-context-selector';
+import { createContext } from 'react';
 import { InputMapping } from './class';
 
 export const InputMappingContext = createContext<InputMapping>(new InputMapping());
